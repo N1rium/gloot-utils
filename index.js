a003f9b947265e72d2d7cb06fb2638c384512234
Tommy Wallberg <tommy.wallberg@gumbler.com>
2018-09-02 12:56:57 +0200

    Change to success so it turns up in slack

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/slack/glogin', function(req, res) {
   const signature = req.headers['X-Slack-Signature'];
 
   if (new Date().getTime() - (timestamp * 1000) > 60 * 5) {
-    res.status(500).json({text : "Replaying not allowed"});
+    res.status(200).json({text : "Replaying not allowed"});
     return;
   }
   
@@ -48,7 +48,7 @@ app.post('/slack/glogin', function(req, res) {
   const expectedSignature = sha256(string);
 
   if (signature != expectedSignature) {
-    res.status(500).json({text : "Invalid signature"});
+    res.status(200).json({text : "Invalid signature"});
     return;
   }
 
